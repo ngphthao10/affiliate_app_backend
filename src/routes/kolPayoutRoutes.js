3576bc0a240b6b8cd7f7df1306ef0d909ea60822
Phuong Thao <ngphthao031028@gmail.com>
2025-04-23 20:44:31 +0700

    update payouts

--- src/routes/kolPayoutRoutes.js
+++ src/routes/kolPayoutRoutes.js
@@ -7,21 +7,26 @@ const {
     getPayoutDetails,
     getInfluencerPayouts,
     getInfluencerPayoutDetails,
-    getInfluencerSalesStats
+    getInfluencerSalesStats,
+    getEligiblePayouts
 } = require('../controllers/kolPayoutController');
 const adminAuth = require('../middlewares/adminAuth');
 const kolAuth = require('../middlewares/kolAuth')
 
 const router = express.Router();
 
-router.get('/list', adminAuth, getPayouts);
+
+router.get('/', adminAuth, getPayouts);
+router.get('/eligible', adminAuth, getEligiblePayouts);
 router.get('/export', adminAuth, exportPayoutReport);
-router.post('/generate', adminAuth, generatePayouts);
-router.patch('/:payout_id/status', adminAuth, updatePayoutStatus);
 router.get('/:payout_id', adminAuth, getPayoutDetails);
+router.post('/generate', adminAuth, generatePayouts);
+router.put('/:payout_id/status', adminAuth, updatePayoutStatus);
+
 
 router.get('/influencer/payouts', kolAuth, getInfluencerPayouts);
 router.get('/influencer/payouts/:payout_id', kolAuth, getInfluencerPayoutDetails);
 router.get('/influencer/stats', kolAuth, getInfluencerSalesStats);
+
 
 module.exports = router;
